257798973f8026b644ad62f1f55c683c3d8fd753
ViVaLaFlame <amir.kakharov2001@gmail.com>
2023-03-19 14:57:03 +0300

    Итоговое звдвние(Модуль7)

---
+++ ReadMyFile/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ReadMyFile
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string filePath = "C:/Users/ТехноСПБ/Desktop/BinaryFile.bin";
+
+            // при запуске проверим, что файл существует
+            if (File.Exists(filePath))
+            {
+                // строковая переменная, в которую будем считывать данные
+                string stringValue;
+
+                // считываем, после использования высвобождаем задействованный ресурс BinaryReader
+                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                {
+                    stringValue = reader.ReadString();
+                }
+
+                // Вывод
+                Console.WriteLine("Из файла считано:");
+                Console.WriteLine(stringValue);
+            }
+            
+            
+        }
+    }
+}    
